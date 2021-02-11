88fdc0acb026c39fab45db88b2adcf71a54c7c71
André Pessoa <pessoam.andre@gmail.com>
2021-02-11 23:45:46 +0000

    First commit

---
+++ public/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>A. Pessoa</title>
+        <meta charset="UTF-8">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Bowlby+One+SC&family=Comic+Neue:wght@700&family=Fredericka+the+Great&family=Fredoka+One&display=swap" rel="stylesheet">
+        <link href="./resources/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <section>
+            <h1>A. Pessoa</h1>  
+            <div id="island">
+                <?php include("./resources/ilha.svg"); ?>
+            </div> 
+            <div id="greeting">
+            <p >Hi! Nice to see you here.</p>
+            <p class="small">(Here it was really lonely.)</p>
+            </div> 
+        </section>
+        
+        <section>
+            <div id="driving">
+                <?php include("./resources/driving.svg"); ?>
+            </div> 
+            <div id="info">
+                <h2>André Pessoa.</h2>
+                <p>I'm a brazilian creative developer, and a formed designer & animator.</p>
+                <p>If you want to know a little more about me, take a look on my profile.</p>
+                <p><a href="https://www.linkedin.com/in/andrepessoamendes/" rel=”noreferrer”>Linkedin</a></p>
+                <br>
+                <p>Thanks oy for coming so depth.</p>
+                <p>I hope see you soon again! = )</p>
+            </div>
+        </section>
+        <script src="./resources/script.js" ></script>
+    </body>
+</html>
---
+++ public/resources/script.js
@@ -0,0 +1,83 @@
+// Animate water line
+const islandTopElem = document.querySelector("#sand-high");
+const islandTopPoints = islandTopElem.getAttribute('d');
+const islandTopValues = getPathValues(islandTopPoints);
+islandTopElem.style= 'opacity: 0';
+
+const islandBottomElem = document.querySelector("#sand-low");
+const islandBottomPoints = islandBottomElem.getAttribute('d');
+const islandBottomValues = getPathValues(islandBottomPoints);
+islandBottomElem.style= 'opacity: 0.05';
+
+const islandAnimatedElem = document.querySelector("#sand-animated");
+const islandAnimationOffset = offsetPathsValues(islandTopValues, islandBottomValues);
+
+function getPathValues(d){
+  return d.split(/(?=[a-zA-Z,-])|(?<=[a-zA-Z,-])/gi);
+}
+
+function offsetPathsValues(a, b) {
+  return a.map((val, i) => {
+    if(!isNaN(val)){
+      return parseFloat(a[i]) - parseFloat(b[i]);
+    }
+    return undefined;
+  });
+}
+
+function getCycleProportion(frame, cycle){
+  return ((frame % cycle) / cycle);
+}
+
+function getSinProportion(frame, cycle){
+  const turn = getCycleProportion(frame, cycle);
+  const rad  = turn * 2 * Math.PI;
+  const prop = (Math.sin(rad) + 1) / 2 * 0.8 + .2;
+
+  return prop;
+}
+
+function drawSeaWave(frame) {
+  const prop1 = getSinProportion(frame, 400);
+  const prop2 = getSinProportion(frame, 300);
+  const prop = prop1 * prop2;
+
+  const offset = islandAnimationOffset.map(val => val !== undefined ? val * prop : val);
+  const pathValues = offset.map((val, i) => val !== undefined ? Math.max((parseFloat(islandBottomValues[i]) + val),0).toFixed(2) : islandTopValues[i]);
+
+  islandAnimatedElem.setAttribute('d', pathValues.join(''));
+}
+
+
+// animate underwater effect
+
+const meDrivingElem = document.querySelector("#driving svg");
+const meDrivingEffect = document.querySelector("#sea-filter");
+
+function drawDrivingEffect(frame) {
+  const cycle = 2000;
+  const noiseProp = getSinProportion(frame + cycle / 4, cycle / 3);
+  const prop1 = getSinProportion(frame, cycle) * noiseProp;
+  const prop2 = getSinProportion(frame + cycle / 2, cycle) * noiseProp;
+  const prop3 = getSinProportion(frame, cycle * 2);
+
+  meDrivingEffect.setAttribute('baseFrequency', (`${0.005 * prop1} ${0.005 * prop2}`));
+  meDrivingElem.setAttribute('style', (`transform: translate(0, ${(50 * prop2).toFixed(2)}px) rotate(${(5 * prop3).toFixed(2)}deg);`));
+}
+
+// animation core
+
+let frame = 0;
+let paused = false;
+function tick(){
+  frame++;
+  try {
+    drawSeaWave(frame);
+    drawDrivingEffect(frame);
+  } catch (error) {
+    paused = true
+  }
+  if(!paused) window.requestAnimationFrame(tick);
+}
+tick();
+
